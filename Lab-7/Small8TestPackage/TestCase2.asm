0e281508bb882dc8c41bc97d57f69c6a122586fc
Nick <nimamshah@ufl.edu>
2016-04-10 21:06:32 -0400

    MIF and ASM work

--- Lab-7/Small8TestPackage/TestCase2.asm
+++ Lab-7/Small8TestPackage/TestCase2.asm
@@ -11,17 +11,17 @@ TWO		EQU	$02
 
 BEGIN:
 	LDAI	TWO		* A = 2
-	STAA	COUNT 		* COUNT = 2
+	STAA	COUNT 	* COUNT = 2
 AGAIN:
-	LDAA	VALUE		* A = VALUE (begins at $FF)
+	LDAA	VALUE	* A = VALUE (begins at $FF)
 	CLRC			* clear carry
 	RORC			* shift A right (alias to VALUE)
-	STAA    VALUE		* store shifted A back into VALUE, now VALUE has been shifted to the right
-	LDAA	COUNT		* A = COUNT
+	STAA    VALUE	* store shifted A back into VALUE, now VALUE has been shifted to the right
+	LDAA	COUNT	* A = COUNT
 	DECA			* A = A - 1
-	STAA	COUNT		* COUNT = A (effectively count = count-1 in the last three instructions)
-	BNEA	AGAIN		* 
-	LDAA    VALUE		* 
+	STAA	COUNT	* COUNT = A (effectively count = count-1 in the last three instructions)
+	BNEA	AGAIN	* 
+	LDAA    VALUE	* 
 	STAA	OUTPORT0
 
 * Termination loop for hardware machine
